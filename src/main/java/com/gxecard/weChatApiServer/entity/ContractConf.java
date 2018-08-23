24d59ca725b5468739d36a9954ec6d6b6272aaac
jerryfu <jrhu05@126.com>
2018-08-23 18:00:26 +0800

    增加小程序配置实体类

--- src/main/java/com/gxecard/weChatApiServer/entity/ContractConf.java
+++ src/main/java/com/gxecard/weChatApiServer/entity/ContractConf.java
@@ -1,7 +1,22 @@
 package com.gxecard.weChatApiServer.entity;
 
-import javax.persistence.Entity;
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
 
+/**
+ * 小程序配置实体类
+ */
 @Entity
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@Table(name = "contract_conf")
+@DynamicUpdate
 public class ContractConf {
+    @Id
+    @GeneratedValue
+    private int confId;
+    private String confName;
+    private String confDesc;
 }
