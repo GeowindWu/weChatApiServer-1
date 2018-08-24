d83592f49819fbbf7d60a6da6dc616744b699411
jerryfu <jrhu05@126.com>
2018-08-24 16:24:11 +0800

    修改打包和启动方式为war

--- src/main/java/com/gxecard/weChatApiServer/WeChatApiServerApplication.java
+++ src/main/java/com/gxecard/weChatApiServer/WeChatApiServerApplication.java
@@ -2,9 +2,16 @@ package com.gxecard.weChatApiServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WeChatApiServerApplication {
+public class WeChatApiServerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(WeChatApiServerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WeChatApiServerApplication.class, args);
